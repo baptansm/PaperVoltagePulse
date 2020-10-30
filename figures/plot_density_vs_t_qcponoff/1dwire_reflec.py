9c1ca3c591a477c99137bfc387a7cb73f476ef32
bat.anselme.m@gmail.com <bat.anselme.m@gmail.com>
2020-10-30 19:30:48 +0100

    figures density reflection

---
+++ figures/plot_density_vs_t_qcponoff/1dwire_reflec.py
@@ -0,0 +1,146 @@
+
+#QCP FOR MPI
+import warnings
+import numpy as np
+#import matplotlib.pyplot as plt
+from scipy.special import erf
+import time as temps
+import pickle
+import kwant
+import tkwant
+import kwant_spectrum
+import functools
+
+t0 = temps.time()
+
+W = 1
+L = 2500
+V0 = 3
+Ef = 2.02
+#Energies = [2.01,2.02,2.03,2.04,2.05,2.06,2.07,2.08]
+u=0
+nbar = 0.01
+tau = 500
+#prm_str = 'W{:d}_L{:d}_V0{:1.1f}_Ef{:1.2f}_u{:1.1f}_n{:1.1f}_tau{:3.1f}'.format(W,L,V0,Ef,u, nbar,tau)
+path = './Plots_MPI/'
+#warnings.simplefilter('ignore') 
+comm = tkwant.mpi.get_communicator()
+
+def am_master():
+    return comm.rank == 0 # cache possibly performance warning
+
+# -------------------------------------------------------------------------------------
+#Make system for TKWANT SIMULATION
+
+def make_system(a = 1, t = 1.0,L=8,W = 1, lx = 50, V0 = 4):
+    lat = kwant.lattice.square(a=1, norbs = 1)
+    syst = kwant.Builder()
+
+
+   # def onsite(site):
+    #    (x,y) = site.pos
+    #    Vx = (np.tanh((x - x0)/lx) + np.tanh(-(x + x0)/lx))*0.5*V0
+   #     Vy = (np.tanh((y - y0)/ly) +  np.tanh(-(y + y0)/ly) +2)*0.5
+     #   Vy =-(1-np.tanh((y-y0)/ly))
+      #  return Vx*Vy + 4*t
+    x0 = L*0.8
+    def onsite(site):
+        (x,y) = site.pos
+        return np.exp(-((x-x0)/lx)**2)*V0 + 4*t
+      # area under voltage pulse
+    
+    time0 = 1200
+    Vp = (nbar / tau) * np.sqrt(2/np.pi)
+    
+    #Vp = 2*(nbar / tau) * np.sqrt(2*np.pi)
+#    Vp = 4 * (nbar / tau) * np.sqrt(np.log(2) * np.pi)
+
+    def gaussian(time):
+        A = Vp*tau*np.pi*np.sqrt(np.pi/2)
+        sigma = tau/np.sqrt(2)
+        phi = A*(1 + erf((time - time0)/sigma))
+        return phi
+        
+    
+
+    syst[(lat(x,y) for x in range(0,L) for y in range(0,W))] = onsite
+    syst[lat.neighbors()] = -t  
+
+    
+    
+    lead = kwant.Builder(kwant.TranslationalSymmetry((-a,0)))
+    lead[(lat(0,y) for y in range(0,W))] = 4*t
+    lead[lat.neighbors()] = -t
+    syst.attach_lead(lead)
+    syst.attach_lead(lead.reversed())
+    added_sites = tkwant.leads.add_voltage(syst, 0, gaussian)
+    
+
+    return syst, added_sites
+
+
+def main(W = 1,L = 200,V0 = 0, Ef = 4):
+
+    # create system
+    syst, added_sites = make_system(W=W,L=L,V0 = V0)
+    syst = syst.finalized()
+    
+    interaction_wavefunc = tkwant.manybody.Interaction(added_sites, u=u, update_error_tol=1E-6)
+    syst = tkwant.manybody.CustomSystem(syst, interaction=interaction_wavefunc)
+    # plot the system and dispersion
+    chemical_potential = Ef
+    tmax = 15000
+    #times = np.concatenate((np.array([0]), np.linspace(800,6000,10)) ,axis = 0)
+    times = np.linspace(0,tmax,28)
+    # define an observable
+   
+    
+    density_operator = kwant.operator.Density(syst)
+
+    # do the actual tkwant simulation
+   # print('occupation')
+    spectra = kwant_spectrum.spectra(syst.leads)
+    occupation = tkwant.manybody.lead_occupation(chemical_potential)
+    #intervals = tkwant.manybody.calc_intervals(spectra, occupation)
+    Interval = functools.partial(tkwant.manybody.Interval, order =25,quadrature = 'gausslegendre' )
+    intervals = tkwant.manybody.calc_intervals(spectra, occupation, interval_type=Interval)
+    intervals = tkwant.manybody.split_intervals(intervals, number_subintervals = 120)
+    tasks = tkwant.manybody.calc_tasks(intervals, spectra, occupation)
+    emin, emax = tkwant.manybody.calc_energy_cutoffs(occupation)
+    boundaries = tkwant.leads.automatic_boundary(spectra, tmax=tmax, emin=emin, emax=emax)
+    psi_init = tkwant.manybody.calc_initial_state(syst, tasks, boundaries)
+    solver = tkwant.manybody.WaveFunction(psi_init, tasks)
+    interaction_wavefunc.set_solver(solver, syst)
+
+    #print('solver fini')
+ 
+
+    charge_density = []
+    #current = []
+    for time in times:
+        
+
+        interaction_wavefunc.evolve(time=time)
+        d= interaction_wavefunc.evaluate(density_operator)
+     #   i= interaction_wavefunc.evaluate(current_operator)
+
+        charge_density.append(d)
+   #     current.append(i)
+        if am_master():
+            print("t =", time)
+            print('max n - n0 =', max(abs(d - charge_density[0])))
+
+
+
+    #charge_density = np.array(charge_density)
+
+    return times, charge_density
+
+
+	
+times, charge_density = main(W = W,L = L ,V0 = V0, Ef = Ef) 
+prm_str = 'W{:d}_L{:d}_V0{:1.1f}_Ef{:1.2f}_u{:1.1f}_n{:1.1f}_tau{:3.1f}'.format(W,L,V0,Ef,u, nbar, tau)
+if am_master():
+    
+    results = (times, charge_density)
+    pickle.dump(results, open('dens_current'+prm_str+'.npy', "wb"))
---
+++ figures/plot_density_vs_t_qcponoff/plot_densities_vs_t_qpc_off_noreflection.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 11 15:24:24 2020
+
+@author: baptiste
+"""
+
+
+import kwant
+import tkwant
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import erf
+import kwant_spectrum
+import functools
+import pickle
+import glob
+from scipy.optimize import curve_fit
+
+def plot_densities(file, W = 1, tmin =0, tmax = 500,imin =10, imax = 1400, fac = 1e7, M =1, title ='') :
+    times , charge_density = pickle.load(open(file, 'rb'))
+
+        
+    #print(times)
+  #Ef = float(file.split('_')[-2].split('Ef')[-1])
+    f, ax = plt.subplots(1,1, figsize = (10,10))
+    f.suptitle(title, fontsize = 20)
+
+    L = int(len(charge_density[0])/W)
+    X = [n for n in range(len(charge_density[0]))]
+    X1 = [n for n in range(L)][imin:imax]
+    N = len(charge_density[0])
+    lines = []
+    T = []
+    i = 0
+    for c, time in zip(charge_density, times):
+        c = c - charge_density[0]
+        c_reduced = []
+        #c -= c[-1]
+        if tmin< time < tmax:
+            T.append(time)
+            for n in range(L):
+                m = [c[n*W+i] for i in range(0,W)]
+                c_reduced.append(np.sum(m, axis = 0)/W)
+               # c_reduced.append(c[n*W + 1])
+            c_reduced = np.array(c_reduced[imin:imax])
+            if i%M == 0:
+           
+                lines.append(ax.plot(X1, c_reduced*fac+ time, color = 'black',
+                                 label = 't = {:2.0f}'.format(time))[0])
+            #ax.axhline(i*offset, linestyle = '--', alpha = 0.3, color = 'black')
+            i+=1
+
+    ax.tick_params(axis= 'both', labelsize = 18)
+    ax.set_xlabel('x', fontsize = '20')
+    ax.set_ylabel('time $t$', fontsize = '20')
+
+files = glob.glob('*V03*')
+plot_densities(files[0], W =1, tmin =1000, tmax = 15000,imax = 2200, fac = 6e6, M =1, title ='') 
+plt.savefig('plot_density_vs_t_qpc_off_noreflection.pdf', format = 'pdf', dpi = 60)
+  
